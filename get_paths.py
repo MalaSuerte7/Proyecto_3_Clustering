a0f76da3146c0e4cd54c82c6e22ba35d193c1109
LoveTrain <lifegiver3456@gmail.com>
2024-10-23 17:33:50 -0500

    Paths_1

--- get_paths.py
+++ get_paths.py
@@ -19,10 +19,28 @@ def pegar_txt(paths , output_txt):
             file.write(path + '\n')
 
 
-# val_features --> paths
-# path --> C:\Users\lifeg\OneDrive\Escritorio\heavy_data\val_subset
-val_videos = "C:\\Users\\lifeg\\OneDrive\\Escritorio\\heavy_data\\val_subset"
-out_val = "C:\\Users\\lifeg\\OneDrive\\Escritorio\\Machine\\Proyecto_3_Clustering\\val_paths.txt"
+# # val_features --> paths
+# # path --> C:\Users\lifeg\OneDrive\Escritorio\heavy_data\val_subset
+# val_videos = "C:\\Users\\lifeg\\OneDrive\\Escritorio\\heavy_data\\val_subset"
+# out_val = "C:\\Users\\lifeg\\OneDrive\\Escritorio\\Machine\\Proyecto_3_Clustering\\val_paths.txt"
+# ## quizas también sin necesidad de ponerlo en un txt se podía hacer pero para visualizarlo mejor que se escriba.
+# allpaths_val = getPaths(val_videos)
+# pegar_txt(allpaths_val, out_val)
+
+
+# # train_features --> paths
+# # path --> C:\Users\lifeg\OneDrive\Escritorio\heavy_data\train_subset 
+# train_videos = "C:\\Users\\lifeg\\OneDrive\\Escritorio\\heavy_data\\train_subset"
+# out_train = "C:\\Users\\lifeg\\OneDrive\\Escritorio\\Machine\\Proyecto_3_Clustering\\train_paths.txt"
+# ## quizas también sin necesidad de ponerlo en un txt se podía hacer pero para visualizarlo mejor que se escriba.
+# allpaths_train = getPaths(train_videos)
+# pegar_txt(allpaths_train, out_train)
+
+
+# test_features --> paths
+# path --> C:\Users\lifeg\OneDrive\Escritorio\heavy_data\test_subset 
+test_videos = "C:\\Users\\lifeg\\OneDrive\\Escritorio\\heavy_data\\test_subset"
+out_test = "C:\\Users\\lifeg\\OneDrive\\Escritorio\\Machine\\Proyecto_3_Clustering\\test_paths.txt"
 ## quizas también sin necesidad de ponerlo en un txt se podía hacer pero para visualizarlo mejor que se escriba.
-allpaths_val = getPaths(val_videos)
-pegar_txt(allpaths_val, out_val)
+allpaths_test = getPaths(test_videos)
+pegar_txt(allpaths_test, out_test)
